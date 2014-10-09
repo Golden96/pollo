a017e483637eaa8aaf53a68cce7df8ef1efe1758
Golden96 <sergio64gr@gmail.com>
2014-10-09 19:14:59 +0200

    Final

---
+++ Assets/Scripts/resetlevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resetlevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D Obstaculo){
+		Application.LoadLevel (Application.loadedLevel);
+}
+}
